0e2efc0b730a4b54e3ac14dbf74502b67eeb3d76
Kushal Dongre <kushz19@gmail.com>
2016-05-30 21:06:34 +0530

    product

---
+++ app/views/products/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @product, :id, :name, :price, :quantity, :image, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160530144402) do
+
+  create_table "products", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.string   "price",      limit: 255
+    t.integer  "quantity",   limit: 4
+    t.string   "image",      limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
 end
